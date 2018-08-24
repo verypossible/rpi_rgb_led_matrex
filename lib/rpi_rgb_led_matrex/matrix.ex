4d8dc72698b3f90af6e5a34d5333bd19becd66c0
Jacob <realshin@gmail.com>
2018-08-24 10:03:58 -0400

    Use module name
    Why:
    
    * Rather than tracking PIDs around
    
    This change addresses the need by:
    
    * Using `__MODULE__`
    * Removing PIDs

--- lib/rpi_rgb_led_matrex/matrix.ex
+++ lib/rpi_rgb_led_matrex/matrix.ex
@@ -18,58 +18,50 @@ defmodule RpiRgbLedMatrex.Matrix do
    * `opts` are any options to pass to GenServer.start_link
   """
   @spec start_link([term]) :: {:ok, pid}
-  def start_link(opts \\ []) do
+  def start_link(opts \\ [name: __MODULE__]) do
     GenServer.start_link(__MODULE__, :ok, opts)
   end
 
   @doc """
   Stop the GenServer and release the Matrix resources.
-
-  Parameters:
-   * `pid` the process id of the GenServer that is being terminated
   """
-  @spec release(pid) :: :ok
-  def release(pid) do
-    GenServer.cast(pid, :release)
+  @spec release() :: :ok
+  def release() do
+    GenServer.cast(__MODULE__, :release)
   end
 
   @doc """
   Sets one led of the matrix to a color value.
-  
+
   Parameters:
-   * `pid` the process id of the GenServer that is being terminated
    * `color` a tuple representing the `r`, `g`, and `b` values of the color to set the pixel
    * `point` a tuple representing the `x` and `y` coordinates of the pixel to be set
   """
-  @spec set_pixel(pid, {integer, integer, integer}, {integer, integer}) :: :ok
-  def set_pixel(pid, color, point) do
+  @spec set_pixel({integer, integer, integer}, {integer, integer}) :: :ok
+  def set_pixel(color, point) do
     padded_color = pad_value(color, 3)
     padded_point = pad_value(point, 2)
     data = padded_color ++ padded_point
-    GenServer.cast(pid, {:set_pixel, data})
+    GenServer.cast(__MODULE__, {:set_pixel, data})
   end
 
   @doc """
   Fills the whole matrix of leds with the provided color value.
 
   Parameters:
-   * `pid` the process id of the GenServer that is being terminated
    * `color` a tuple representing the `r`, `g`, and `b` values of the color to fill the matrix
   """
-  @spec fill(pid, {integer, integer, integer}) :: :ok
-  def fill(pid, color) do
-    GenServer.cast(pid, {:fill, pad_value(color, 3)})
+  @spec fill({integer, integer, integer}) :: :ok
+  def fill(color) do
+    GenServer.cast(__MODULE__, {:fill, pad_value(color, 3)})
   end
 
   @doc """
   Clears the whole matrix of leds.
-
-  Parameters:
-   * `pid` the process id of the GenServer that is being terminated
   """
-  @spec clear(pid) :: :ok
-  def clear(pid) do
-    GenServer.cast(pid, :clear)
+  @spec clear() :: :ok
+  def clear() do
+    GenServer.cast(__MODULE__, :clear)
   end
 
   # gen_server callbacks
@@ -113,7 +105,6 @@ defmodule RpiRgbLedMatrex.Matrix do
 
   defp pad_value(values, max_length) do
     Tuple.to_list(values)
-      |> Enum.map(fn(val) -> Integer.to_string(val) |> String.pad_leading(max_length, "0") end)
+    |> Enum.map(fn val -> Integer.to_string(val) |> String.pad_leading(max_length, "0") end)
   end
 end
-
